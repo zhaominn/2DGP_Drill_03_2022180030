af471ad481c30421c3892197e176caa4b653fb4a
zhaominn <somin207@naver.com>
2024-09-24 10:19:53 +0900

    원 운동 그리기 완료

--- character_grass.py
+++ character_grass.py
@@ -8,16 +8,25 @@ boy = load_image('character.png')
 
 hide_lattice()
 
+
 def run_rectangle():
-    print('RECTANGLE') 
+    print('RECTANGLE')
+    pass
+
 
 def run_circle():
     print('CIRCLE')
+    r, cx, cy = 300, 800 // 2, 600 // 2
+    for d in range(0,360):
+        x=r*math.cos(math.radians(d))+cx
+        y=r*math.sin(math.radians(d))+cy
 
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(1)
-    
+        clear_canvas_now()
+        boy.draw_now(x,y)
+        delay(0.1)
+
+    pass
+
 
 while(True):
     run_circle()
